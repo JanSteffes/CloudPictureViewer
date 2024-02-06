4bb791d780aaf152d4a7a3ae0d3aef209ba86083
Redensarten <jan.steffes@hotmail.de>
2024-02-06 14:21:55 +0100

    Add project files.

---
+++ CloudPictureViewer/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace CloudPictureViewer.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
